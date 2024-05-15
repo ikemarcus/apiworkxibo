a3470f536eaac19c2be2f774966b89136efb34de
ikemarcus <ikemarcus@hotmail.nl>
2024-05-15 14:35:31 +0200

    Create inlog.js

---
+++ inlog.js
@@ -0,0 +1,28 @@
+const users = [
+    { username: "Tom Stommels", password: "Toveren1!" },
+    { username: "Ike Marcus", password: "Toveren1!" }
+];
+
+const app = Vue.createApp({
+    data() {
+        return {
+            username: "",
+            password: "",
+            loginError: false
+        };
+    },
+    methods: {
+        login() {
+            const { username, password } = this;
+            const user = users.find(u => u.username === username && u.password === password);
+
+            if (user) {
+                window.location.href = "page.html"; 
+            } else {
+                this.loginError = true;
+            }
+        }
+    }
+});
+
+app.mount('#loginForm');
